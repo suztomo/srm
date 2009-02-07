d65347d545069eb5f320c8ccb8597cd96ce31cbb
suztomo <suztomo@gmail.com>
2009-02-07 17:27:00 +0900

    first commit

---
+++ cout.h
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+using namespace std;
+
+ostream& operator<<(ostream &s, vector<string> v)
+{
+  int cnt = v.size();
+  s << "[ ";
+  for (int i=0; i<cnt; i++) {
+	if (i > 0) s << ", ";
+	s << '"' << v[i] << '"';
+  }
+  return s << " ]  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T> ostream& operator<<(ostream &s, vector<T> v)
+{
+  int cnt = v.size();
+  s << "[ ";
+  for (int i=0; i<cnt; i++) {
+	if (i > 0) s << ", ";
+	s << v[i];
+  }
+  return s << " ]  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T> ostream& operator<<(ostream &s, list<T> ls)
+{
+  int cnt = 0;
+  s << "( ";
+  for (typeof(ls.begin()) it=it.begin(); it!=it.end(); it++) {
+	if (it != it.begin()) s << ", ";
+	s << *it;
+	cnt++;
+  }
+  return s << " )  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T> ostream& operator<<(ostream &s, deque<T> st)
+{
+  int cnt = st.size();
+  s << "[ ";
+  for (typeof(st.begin()) it=st.begin(); it!=st.end(); it++) {
+	if (it != st.begin()) s << ", ";
+	s << *it;
+  }
+  return s << " ]  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T1, typename T2> ostream& operator<<(ostream &s, map<T1,T2> m)
+{
+  int cnt = m.size();
+  s << "{ ";
+  for (typeof(m.begin()) it=m.begin(); it!=m.end(); it++) {
+	if (it != m.begin()) s << ", ";
+	s << it->first << " => " << it->second;
+  }
+  return s << " }  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T> ostream& operator<<(ostream &s, set<T> st)
+{
+  int cnt = st.size();
+  s << "[ ";
+  for (typeof(st.begin()) it=st.begin(); it!=st.end(); it++) {
+	if (it != st.begin()) s << ", ";
+	s << *it;
+  }
+  return s << " ]  // " << cnt << " item" << (cnt >= 2 ? "s" : "");
+}
+
+template <typename T1, typename T2> ostream& operator<<(ostream &s, pair<T1,T2> p)
+{
+  return s << "(" << p.first << "," << p.second << ")";
+}
